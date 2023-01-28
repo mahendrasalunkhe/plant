6ec3ec690cecc6be32f329317e4dc9ad08397d17
mahendrasalunkhe <80094703+mahendrasalunkhe@users.noreply.github.com>
2023-01-29 02:38:23 +0530

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,12 @@ import streamlit as st
 
 hide_st_style = """
 <style>
-header {visibility: visible;}
+header {visibility: hidden;}
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
 
 footer:after {
-   content:'© Wind World (India) Limited, Developed by SCADA'; 
+   content:'©Vertical Plant'; 
    visibility: visible;
    display: block;
    position: relative;
@@ -16,13 +16,7 @@ footer:after {
    top: 2px;
    text-align: center;
 }
-.css-uc1cuc{
-  height: 0rem;
-}
 
-.css-18e3th9 {
-    padding: 1rem
-}
 
 .stApp {
     background-image: url("https://img.wallpapersafari.com/desktop/1600/900/82/66/paOWM4.jpg");
@@ -31,24 +25,20 @@ footer:after {
 </style>
 """
 
-#st.set_page_config(page_title="Plant", layout="wide", page_icon=":shark:", initial_sidebar_state="collapsed")
-import streamlit as st
-
 st.set_page_config(
     page_title="Vertical Plant",
-    page_icon="🌱",
+    page_icon="https://raw.githubusercontent.com/mahendrasalunkhe/plant/main/71020327.png?token=GHSAT0AAAAAAB6A64ERGCGTOLDG2GDMYCJGY6VRCGQ",
     layout="wide",
     initial_sidebar_state="collapsed",
     menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# vertical plant creator!"
+        'About': "# Vertical plant creator!"
     }
 )
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 head_col1, head_col2, head_margin = st.columns((1, 1, 4))
-head_col1.image("https://github.com/mahendrasalunkhe/plant/blob/main/logo.jpg")
-new_title = '<p style="font-family:sans-serif; color:darkgreen; font-size: 36px;"><b> Vertical Earth</b></p>'
+
+head_col1.image("https://raw.githubusercontent.com/mahendrasalunkhe/plant/main/71020327.png?token=GHSAT0AAAAAAB6A64ERGCGTOLDG2GDMYCJGY6VRCGQ")
+new_title = '<p style="font-family:sans-serif; color:darkgreen; font-size: 36px;"><b>Etha Vertical Earth</b></p>'
 head_col2.markdown(new_title, unsafe_allow_html=True)
 
