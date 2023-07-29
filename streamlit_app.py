4a96b9088c60d1951cc1ae0cebd40a2ebf95379e
mahendrasalunkhe <80094703+mahendrasalunkhe@users.noreply.github.com>
2023-07-29 15:02:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ st.set_page_config(
 )
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
-head_col1, head_col2, head_margin = st.columns((0.3, 0.7, 5))
+head_col1, head_col2, head_margin = st.columns((0.3, 1.7, 4))
 
 head_col1.image("https://raw.githubusercontent.com/mahendrasalunkhe/plant/main/etha-earth-logo.png?token=GHSAT0AAAAAAB6A64EQU6FTCLAC4KQUJNPKY6VR5ZQ")
 new_title = '<p style="font-family:sans-serif; color:darkgreen; font-size: 36px;"><b>Etha Earth</b></p>'
