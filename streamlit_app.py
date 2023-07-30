f4d24df4a069a8dece94fcde5745ada7db3ad6bd
mahendrasalunkhe <80094703+mahendrasalunkhe@users.noreply.github.com>
2023-07-31 01:38:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 head_col1, head_col2, head_margin = st.columns((0.3, 2.7, 3), use_container_width=True)
 head_col1.image("https://raw.githubusercontent.com/mahendrasalunkhe/plant/main/etha-earth-logo.png?token=GHSAT0AAAAAAB6A64EQU6FTCLAC4KQUJNPKY6VR5ZQ")
 logo_name = '<p style="font-family:sans-serif; color:Orange; font-size: 36px;"><b>Etha-earthpallet</b></p>'
-head_col2.markdown(logo_name, unsafe_allow_html=True, use_container_width=True)
+head_col2.markdown(logo_name, unsafe_allow_html=True)
 
 content = """
 <p style="font-family:sans-serif; color:darkgreen; font-size: 12px;">Discover a Green Balcony Oasis! Let our premium grass solutions transform your balcony into a lush, low-maintenance space. Embrace nature and create a relaxing outdoor space to unwind.</p>
@@ -47,8 +47,7 @@ content = """
 
 <p style="font-family:sans-serif; color:darkyellow; font-size: 12px;">Experience the beauty of nature right at your doorstep today!</p>
 <p style="font-family:sans-serif; color:red; font-size: 12px;">Mobile: 7208005001</p>
-<p style="font-family:sans-serif; color:Orange; font-size: 12px;">Email: mahendra.salunkhe@gmail.com</p>
-"""
+<p style="font-family:sans-serif; color:Orange; font-size: 12px;">Email: mahendra.salunkhe@gmail.com</p> """
 st.markdown(content, unsafe_allow_html=True)
 
 # st.caption(':tomato[Discover a Green Balcony Oasis! Let our premium grass solutions transform your balcony into a lush, low-maintenance space. Embrace nature and create a relaxing outdoor space to unwind.]') 
