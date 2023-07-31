c9ed25c0d2ed44078638a5ecb1f0c777e5a6dc8d
mahendrasalunkhe <80094703+mahendrasalunkhe@users.noreply.github.com>
2023-08-01 01:35:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ head_col2.markdown(logo_name, unsafe_allow_html=True)
 content = """
 <p style="font-family:sans-serif; color:#101010; font-size: 15px;">Discover a Green Balcony Oasis! Let our premium grass solutions transform your balcony into a lush, low-maintenance space. Embrace nature and create a relaxing outdoor space to unwind.</p>
 <p style="font-family:sans-serif; color:#101010; font-size: 15px;">Our dedicated team of experts is passionate about greening urban areas. With their guidance and installation services, turning your balcony into a green paradise is easy.</p>
-<p style="font-family:sans-serif; color:#101010; font-size: 15px;">Experience the beauty of nature right at your doorstep today!<br>Mobile: 7208005001<br>Email: mahendra.salunkhe@gmail.com</p>
+<p style="font-family:sans-serif; color:#101010; font-size: 15px;">Experience the beauty of nature right at your doorstep today!<br>Mobile:+91 7208005001<br>Email: mahendra.salunkhe@gmail.com<br>Address: Borivali, Mumbai, India</p>
 """
 head_col3.markdown(content, unsafe_allow_html=True)
 
