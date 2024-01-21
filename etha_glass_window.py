d0c254bdd5790bbc93782ac85f7aead58da804f8
mahendrasalunkhe <80094703+mahendrasalunkhe@users.noreply.github.com>
2024-01-21 13:10:22 +0000

    cc

--- etha_glass_window.py
+++ etha_glass_window.py
@@ -51,10 +51,8 @@ content = """
 <p style="font-family:sans-serif; color:#101010; font-size: 16px;">We believe that this approach allows for greater customization and adds a touch of elegance to any space. With our wide range of vibrant colours and carefully crafted patterns, you can create a truly unique and personalized environment.</p>
 <p style="font-family:sans-serif; color:#101010; font-size: 16px;">Our team will work closely with you to understand your needs and provide tailored solutions that exceed your expectations. Windows is meticulously designed to seamlessly integrate with your existing décor and architectural style.</p>
 <p style="font-family:sans-serif; color:#101010; font-size: 16px;">Ready to transform your space with our glass windows!<br>Mobile:+91 7208005001<br>Email: mahendra.salunkhe@gmail.com<br>Address: Borivali, Mumbai, India.</p>
-
 """
 head2_col1.markdown(content, unsafe_allow_html=True)
-
 
 content_1 = """
 <p style="font-family:sans-serif; color:#101010; font-size: 15px;">Explore our website to discover our extensive range of glass sliding windows. From residential to commercial applications, we offer a variety of sizes, shapes, and configurations to suit your specific requirements.</p>
